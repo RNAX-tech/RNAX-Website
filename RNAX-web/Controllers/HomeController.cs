24afa7bf50ac36172999a9bf0bf5177c641247a0
Neeraja Narayanswamy <f20160225@dubai.bits-pilani.ac.in>
2020-08-02 15:01:12 +0400

    Added Login Page to Controller

--- RNAX-web/Controllers/HomeController.cs
+++ RNAX-web/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace RNAX_web.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
