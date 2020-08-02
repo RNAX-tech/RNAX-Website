f1ebec2a5d91aa5f44eeb5927f54d9a4f0c2dd87
Neeraja Narayanswamy <f20160225@dubai.bits-pilani.ac.in>
2020-08-02 20:03:11 +0400

    Constructing Login Infrastructure

--- RNAX-web/Models/AppDbContext.cs
+++ RNAX-web/Models/AppDbContext.cs
@@ -4,15 +4,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Xamarin.Essentials;
 
 namespace RNAX_web.Models
 {
-    public class User
+    public class AppDbContext : IdentityDbContext
     {
-        public string Email { get; set; }
-        public string password { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
 
-        public Project projects { get; set; }
+        }
+
     }
 }
