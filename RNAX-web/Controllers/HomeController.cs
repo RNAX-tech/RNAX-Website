97da10f9d0f7a1da5adfc5043b749719c817794d
Neeraja Narayanswamy <f20160225@dubai.bits-pilani.ac.in>
2020-08-03 06:50:27 +0400

    User Repository added to home controller

--- RNAX-web/Controllers/HomeController.cs
+++ RNAX-web/Controllers/HomeController.cs
@@ -12,10 +12,12 @@ namespace RNAX_web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUserRepository _userRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IUserRepository userRepository)
         {
             _logger = logger;
+            _userRepository = userRepository;
         }
 
         public IActionResult Index()
