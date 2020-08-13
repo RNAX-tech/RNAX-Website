bcfc289c61a79c66031d39d7b0bf4505c355f7a2
VijayaGB98 <vijaya.gajanan@outlook.com>
2020-08-13 11:24:40 +0400

    Modified the background

--- RNAX-web/Controllers/HomeController.cs
+++ RNAX-web/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace RNAX_web.Controllers
             return View();
         }
 
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
